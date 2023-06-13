17a9299e5179fbc3fe73efe2ba07216ce039f2c3
Ramses <127460936+ramsesgithub@users.noreply.github.com>
2023-06-13 09:22:09 -0300

    empezando js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,7 @@
 //Haz tú validación en javascript acá
+const form = document.querySelector(".formcontato__form");
+const inputName = document.querySelector(".formcontato__input");
+form.addEventListener("submit",e=>{
+    e.preventDefault() //evitar que se recargue la pagina
+    if(inputName.value.length > 50)console.log("error");
+})
